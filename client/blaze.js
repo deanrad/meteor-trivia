652c0048bf9c4b52f193a86a81f8c0fc8606afe9
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-08-26 16:32:27 -0500

    2nd impl - using a reactive var
    note with this approach you can't have 2 instances of template they'll
    conflict

--- client/blaze.js
+++ client/blaze.js
@@ -1,6 +1,39 @@
+import { ReactiveVar } from 'meteor/reactive-var'
 import { Template } from 'meteor/templating'
+import Rx from 'rx'
 import './blaze.html'
 
+
+const observable = Rx.Observable.create(observer => {
+  observer.onNext(1)
+
+  setTimeout(() => {
+    observer.onNext(2)
+  }, 1000)
+
+  // observer.onNext(2)
+  setTimeout(() => {
+    observer.onNext(3)
+    observer.onCompleted()
+  }, 2000)
+
+  // can be invoked as dispose()
+  return () => {}
+})
+
+
+const rxToReactiveVar = (rx) => {
+  let rvar = new ReactiveVar()
+  rx.subscribe(next => rvar.set(next))
+  return rvar
+}
+
+let reactiveObservable
+Template.blaze.onCreated(() => {
+  // var self = this
+  reactiveObservable = rxToReactiveVar(observable)
+})
+
 Template.blaze.helpers({
-  latestOfObservable: () => 2
+  latestOfObservable: () => reactiveObservable.get()
 })
