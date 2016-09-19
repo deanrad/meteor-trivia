c0ef586981b53e79ca5b5036d9256d7a2f689614
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-09-19 17:24:14 -0500

    Pruning down

--- .githooks/pre-commit/quality.sh
+++ .githooks/pre-commit/quality.sh
@@ -1,2 +1,3 @@
-npm run lint && npm run test:fast
+npm run test:fast
+# npm run lint && npm run test:fast
 # not test:acceptance, since it requires a running browser
--- imports/server/publications/serverActions.js
+++ imports/server/publications/serverActions.js
@@ -5,30 +5,18 @@ import clientUpdatesStream, { sanitizePayload } from '../streams/clientUpdatesSt
 
 // Note - Mergebox will not publish events that are dupes of previous ones, thus the
 // inclusion of always-unique ObjectIDs on messages
-createPublication('serverActions', ({ connection, added, error, ready, onStop }) => {
-  console.log(`got subscriber ${connection.id}, sent state`,
+Meteor.publish('serverActions', function() {
+  var client = this
+
+  console.log(`got subscriber ${client.connection.id}, sent state`,
     JSON.stringify(sanitizePayload(store.getState()).toJS()))
 
-  added('serverActions', new Mongo.ObjectID(),
+  client.added('serverActions', new Mongo.ObjectID(),
     {
       type: 'RESET',
       payload: sanitizePayload(store.getState()).toJS(),
       meta: { fromServer: 1 }
     })
 
-  // each subscriber becomes a listener
-  let subscription = clientUpdatesStream.subscribe((action) => {
-    console.log(`sending action upstream to ${connection.id}`, action.toJS())
-    added('serverActions',
-      new Mongo.ObjectID(),
-      { ...action.toJS(), meta: { fromServer: 1 } }
-    )
-  }, error)
-
-  onStop(() => {
-    console.log(`${connection.id} has said bye-bye`)
-    subscription.dispose()
-  })
-
-  ready()
+  client.ready()
 })
