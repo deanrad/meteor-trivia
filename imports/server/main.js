bb3ae724b289346648a1dcb5851d5b8142583192
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-10-19 20:59:00 -0500

    Remove presently unused storeStateStream

--- imports/server/main.js
+++ imports/server/main.js
@@ -1,6 +1,5 @@
 import '../methods/server/dispatchAction'
 import './publications/processedActions'
 import '../server/listeners/dispatchActionsToStore'
-import '../server/streams/storeStateStream'
 
 // absolute paths dont appear on dependency graphs, these are just for startup
--- imports/store/store.js
+++ imports/store/store.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor'
-import { _ } from 'meteor/underscore'
 import { createStore, applyMiddleware } from 'redux'
 import { Map } from 'immutable'
 import rootReducer from './reducers/root'
@@ -13,26 +12,22 @@ if (Meteor.isClient) {
   }
 }
 
-// client and server include these middlewares,
-const middlewares = [...(_.values(RoundEpics))]
+// Both client and server include these middlewares
+const middlewares = [RoundEpics.hinter]
 
-// As the last middleware in the stack, allows you to see 'expanded' stream of actions,
-// Useful for when middlewares alter the incoming stream of actions
-const maybeAddServerMiddlewares = () => {
-  if (Meteor.isServer) {
+// On the server, where we expand actions, we publish a stream of actions about to
+// be dispatched to the store, exposing them to publications, etc..
+if (Meteor.isServer) {
     // eslint-disable-next-line global-require
-    let { pushNext } = require('../server/streams/expandedActions')
-
-    // eslint-disable-next-line no-unused-vars
-    const streamer = store => next => action => {
-      pushNext(action)
-      return next(action)
-    }
-
-    middlewares.push(streamer)
-  }
+  let { pushNext } = require('../server/streams/expandedActions')
+  // As the last middleware in the stack, allows you to see 'expanded' stream of actions,
+  // Useful for when middlewares alter the incoming stream of actions
+  // eslint-disable-next-line no-unused-vars
+  middlewares.push(store => next => action => {
+    pushNext(action)
+    return next(action)
+  })
 }
-maybeAddServerMiddlewares()
 
 export default storeFactory(rootReducer,
   new Map(),
